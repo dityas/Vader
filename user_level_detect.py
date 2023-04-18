b0b93bf0daffaa1d574f8e5b37d51fa84b85999b
Aditya Shinde <shinde.aditya386@gmail.com>
2023-04-18 13:24:23 -0400

    add check for /dev

--- user_level_detect.py
+++ user_level_detect.py
@@ -1,4 +1,5 @@
 import subprocess
+import pathlib
 
 
 def _get_interface_addrs(raw_strings):
@@ -22,5 +23,17 @@ def check_known_MAC_ifaces():
     print("[ ] No known VM ifaces found")
 
 
+def check_known_dev_files():
+    known_dirs = set(["vboxuser", "vboxguest"])
+
+    for dev in pathlib.Path("/dev").iterdir():
+        if dev.parts[-1] in known_dirs:
+            print(f"[X] Found device file {dev} associated with VBox")
+            return True
+
+    print("[ ] No known /dev files found")
+
+
 if __name__ == "__main__":
     check_known_MAC_ifaces()
+    check_known_dev_files()

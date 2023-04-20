87a72ee60858ca9d1831fd4d8b418fc264c9eab3
Aditya Shinde <shinde.aditya386@gmail.com>
2023-04-20 15:36:00 -0400

    kernel module hello world

--- vm-cloak/vm-cloak.c
+++ vm-cloak/vm-cloak.c
@@ -1,3 +1,25 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Aditya Shinde");
+MODULE_DESCRIPTION("Simple syscall interceptor");
+
+
+static int __init start_vm_cloak(void) {
+
+    pr_info("Starting VMCloak\r\n");
+    return 0;
+}
+
+
+static void __exit stop_vm_cloak(void) {
+
+    pr_info("Stopping VMCloak\r\n");
+}
+
+
+module_init(start_vm_cloak);
+module_exit(stop_vm_cloak);

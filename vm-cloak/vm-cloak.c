7b22afc8d23b3547f83f905fd5b81bfa22d32194
Aditya Shinde <shinde.aditya386@gmail.com>
2023-04-21 03:50:52 -0400

    change symbol name to static

--- vm-cloak/vm-cloak.c
+++ vm-cloak/vm-cloak.c
@@ -15,8 +15,9 @@ MODULE_DESCRIPTION("Information spoofer to beat user level virt checks");
 /*
  * Define KProbe for hooking to sys_call_table symbol
  */
+static char symbol[KSYM_NAME_LEN] = "sys_call_table";
 static struct kprobe sys_call_table_probe = {
-    .symbol_name = "sys_call_table",
+    .symbol_name = symbol,
 };
 
 /*

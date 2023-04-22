f8e06c1882a016cf438469f6df30f583fe16fe28
Aditya Shinde <shinde.aditya386@gmail.com>
2023-04-22 15:31:32 -0400

    implement cr0 manipulation in asm

--- vm-cloak/vm-cloak.c
+++ vm-cloak/vm-cloak.c
@@ -18,6 +18,7 @@ static asmlinkage long vm_cloak_openat(int, const char __user *, int, umode_t);
 static unsigned long **syscall_table;
 static unsigned long **get_syscall_table(void);
 
+static inline void asm_write_cr0(unsigned long);
 static void remove_write_prot(void);
 static void restore_write_prot(void);
 
@@ -55,12 +56,20 @@ static void unhook_openat(void) {
 }
 
 
+static inline void asm_write_cr0(unsigned long cr0) {
+    /*
+     * Looks like write_cr0 in version >= 5.10 does not work after
+     * CPU init. So we'll go down to assembly and do it ourselves
+     */
+    asm volatile("mov %0, %%cr0" : "+r"(cr0) :: "memory");
+}
+
 static void restore_write_prot(void) {
 
     unsigned long cr0 = read_cr0();
     pr_err("CR0 value before setting is 0x%lx", cr0);
     set_bit(16, &cr0);
-    write_cr0(cr0);
+    asm_write_cr0(cr0);
     cr0 = read_cr0();
     pr_err("CR0 value after setting is 0x%lx", cr0);
 }
@@ -71,7 +80,7 @@ static void remove_write_prot(void) {
     unsigned long cr0 = read_cr0();
     pr_err("CR0 value before clearing is 0x%lx", cr0);
     clear_bit(16, &cr0);
-    write_cr0(cr0);
+    asm_write_cr0(cr0);
     cr0 = read_cr0();
     pr_err("CR0 value after clearing is 0x%lx", cr0);
 }
